229c16b8b1e8351b5dff7d3da8520ea77c5f5f39
LinoyPeerProject <linoypeer1@gmail.com>
2024-12-05 13:28:23 +0200

    break in postpage at the end

--- src/posts/pages/PostsPage.jsx
+++ src/posts/pages/PostsPage.jsx
@@ -37,6 +37,7 @@ export default function PostsPage() {
                 isLoading={isLoading}
                 error={error}
             />
+            <br></br><br></br><p></p>
         </>
     );
 }
