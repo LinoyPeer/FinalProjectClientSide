cda662c537c4a02c2dab87e83387e3986a1987d6
LinoyPeerProject <linoypeer1@gmail.com>
2025-01-29 20:26:15 +0200

    modified:   src/routes/Router.jsx

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom'
 import ROUTES from './routes'
 import PostsPage from '../posts/pages/PostsPage'
 import AboutPage from '../pages/AboutPage'
-import MyPosts from '../posts/pages/MyPosts'
 import ErrorPage from '../pages/ErrorPage'
 import LoginPage from '../users/pages/LoginPage'
 import MyFavoritePosts from '../posts/pages/MyFavoritePosts'
@@ -25,7 +24,6 @@ export default function Router() {
                 <Route path={ROUTES.POSTS} element={<PostsPage />} />
                 <Route path={ROUTES.ABOUT} element={<AboutPage />} />
                 <Route path={ROUTES.FAVORITES} element={<MyFavoritePosts />} />
-                <Route path={ROUTES.MY_POSTS} element={<MyPosts />} />
                 <Route path={ROUTES.LOGIN} element={<LoginPage />} />
                 <Route path={ROUTES.SIGNUP} element={<SignupForm />} />
                 <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
